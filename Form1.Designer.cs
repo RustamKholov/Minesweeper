af2af5b50f1b23265f65954219bd855cd529669d
RustamHolov <roshfor22@gmail.com>
2025-04-29 13:03:23 +0200

    Building UI
    Add grid layout and update copyright information

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBox1 = new GroupBox();
+            tableGrid = new TableLayoutPanel();
+            groupBox1.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(tableGrid);
+            groupBox1.Location = new Point(11, 249);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(407, 489);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "FieldContainer";
+            // 
+            // tableGrid
+            // 
+            tableGrid.AutoSize = true;
+            tableGrid.BackgroundImageLayout = ImageLayout.Zoom;
+            tableGrid.ColumnCount = 9;
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.Dock = DockStyle.Fill;
+            tableGrid.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tableGrid.Location = new Point(3, 23);
+            tableGrid.Name = "tableGrid";
+            tableGrid.RowCount = 9;
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
+            tableGrid.Size = new Size(401, 463);
+            tableGrid.TabIndex = 0;
+            tableGrid.Paint += tableGrid_Paint;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1024, 768);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TableLayoutPanel tableGrid;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace Minesweeper
+﻿namespace Minesweeper
 {
     public partial class Form1 : Form
     {
@@ -9,7 +9,76 @@ namespace Minesweeper
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            InitializeGameGrid();
+        }
 
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableGrid_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void InitializeGameGrid()
+        {
+            
+            tableGrid.Controls.Clear();
+            tableGrid.ColumnStyles.Clear();
+            tableGrid.RowStyles.Clear();
+            int rows = 9;
+            int cols = 9;
+            tableGrid.RowCount = rows;
+            tableGrid.ColumnCount = cols;
+
+            for (int i = 0; i < cols; i++)
+                tableGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / cols));
+
+            for (int i = 0; i < rows; i++)
+                tableGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / rows));
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Button btn = new Button();
+                    btn.Dock = DockStyle.Fill;
+                    btn.BackColor = Color.LightGray;
+                    btn.Margin = new Padding(0);
+                    btn.Tag = (row, col); // optional
+                    btn.Click += Cell_Click;
+
+                    tableGrid.Controls.Add(btn, col, row);
+                    btn.MouseDown += (s, e) =>
+                    {
+                        if (e.Button == MouseButtons.Right)
+                        {
+                            btn.Text = "💣";
+                        }
+                    };
+                    btn.MouseEnter += (s, e) =>
+                    {
+                        btn.BackColor = Color.Gray;
+                    };
+                    btn.MouseLeave += (s, e) =>
+                    {
+                        btn.BackColor = Color.LightGray;
+                    };
+                }
+            }
+        }
+        private void Cell_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is ValueTuple<int, int> coords)
+            {
+                int row = coords.Item1;
+                int col = coords.Item2;
+                MessageBox.Show($"Clicked cell at ({row}, {col})");
+
+                // TODO: call GameEngine.RevealCell(row, col);
+                // update button UI based on result
+            }
         }
     }
 }
